65e98620c2327c002e20dc7e169fce3eef057b0f
shashankrpeta <shashankreddypeta@usf.edu>
2026-06-03 21:53:02 -0400

    Add: python-string-concatenation - submission-0

---
+++ Python For Beginners/python-string-concatenation/submission-0.py
@@ -0,0 +1,10 @@
+def concatenate(s1: str, s2: str) -> str:
+    return s1+s2
+
+
+
+
+# do not modify below this line
+print(concatenate("He", "llo"))
+print(concatenate("Too ", "long!"))
+print(concatenate("Length", "of10"))
