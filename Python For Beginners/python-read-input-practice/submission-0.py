ebe1c153ea599562634ac3b606d9f2d33e72850a
shashankrpeta <shashankreddypeta@usf.edu>
2026-06-04 18:01:54 -0400

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,11 @@
+def add_two_numbers() -> int:
+    my_list=list(map(int,input().split(",")))
+    return sum(my_list)
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
